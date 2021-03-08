193b426a920595992640ec7786c9254282db22cf
Nikolay Kryukov <nislav.ns@gmail.com>
2021-03-08 16:43:28 +0300

    Server requests rate limiting

--- client/src/components/ToTop.js
+++ client/src/components/ToTop.js
@@ -6,16 +6,14 @@ const ToTop = () => {
   const arrow = (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
       fill="currentColor"
-      class="bi bi-chevron-up"
+      className="bi bi-chevron-up"
       viewBox="0 0 16 16"
-      height="64"
-      width="64"
+      height="32"
+      width="32"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
       />
     </svg>
@@ -39,7 +37,7 @@ const ToTop = () => {
   return (
     <div className="fixed bottom-20 right-10 transition duration-500 ease-in-out">
       {isVisible ? (
-        <div className="bg-gray-900 rounded-lg" onClick={scrollToTop}>
+        <div className="bg-gray-900 rounded-md p-2" onClick={scrollToTop}>
           {arrow}
         </div>
       ) : null}
